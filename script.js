ad78e778e72c349039a06ea3f1b4e2bff31e63b8
agashash <agashraj17@gmai.com>
2024-09-24 20:32:36 +0530

    finish my portfolio

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
-      let currentSlideIndex = 0;
-      const slideInterval = 3000; // 3 seconds
-      const slides = document.querySelectorAll('.slide');
-      const totalSlides = slides.length;
-      
-      function showSlide(index) {
-          if (index >= totalSlides) {
-              currentSlideIndex = 0;
-          } else if (index < 0) {
-              currentSlideIndex = totalSlides - 1;
-          } else {
-              currentSlideIndex = index;
-          }
-      
-          const offset = -currentSlideIndex * 100;
-          document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
-      }
-      
-      function changeSlide(direction) {
-          showSlide(currentSlideIndex + direction);
-      }
-      
-      function autoSlide() {
-          showSlide(currentSlideIndex + 1);
-      }
-      
-      // Initialize slideshow
-      showSlide(currentSlideIndex);
-      setInterval(autoSlide, slideInterval);
-
-
-      document.getElementById('openPdfButton').addEventListener('click', function() {
+       document.getElementById('openPdfButton').addEventListener('click', function() {
         // Path to the PDF file
-       const pdfUrl = 'images/Agash.Cv.pdf';
+       const pdfUrl = 'images/Agash-CV.pdf';
 
         // Open PDF in a new window/tab
          window.open(pdfUrl, '_blank');});
